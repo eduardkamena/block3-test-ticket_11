ce9dd8d5a10cf86f47929d78b05043b7b992cceb
Ed <eduard@kamenskikh.me>
2024-10-09 16:25:54 +0300

    Ticket 11 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Билет 11, задача:
+        // Дан массив пар названий книг и авторов,
+        // необходимо привести каждую пару в строку вида:
+        // «Название книги» Имя Отчество Фамилия автора.
+
+        // Решение 1 через двумерный массив
+        String[][] array = {
+                {"Стив Джобс", "У. Айзексон"},
+                {"Война и мир", "c"},
+                {"11 минут", "П. Коэльо"}
+        };
+
+        for (String[] strings : array) {
+            String title = strings[0];
+            String authorName = strings[1];
+            System.out.println("\"" + title + "\"" + " " + authorName);
+        }
+
+        // разделитель
+        System.out.println();
+
+        // Решение 2 через динамический массив
+        List<Book> bookList = new ArrayList<>(List.of(
+                new Book("Стив Джобс", "У. Айзексон"),
+                new Book("Война и мир", "Ф.М. Достоевский"),
+                new Book("11 минут", "П. Коэльо")
+        ));
+
+        for (Book book : bookList) {
+            System.out.println(book);
+        }
+
     }
+
 }
